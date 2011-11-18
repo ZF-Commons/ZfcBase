fb6d76f696a4b423f5e54e7d32e0e8c32b2fcfaa
Evan Coury <me@evancoury.com>
2011-11-18 09:59:24 -0700

    Added string util
    Currently only supports generating stong random bytes.
    
    May add more later.

---
+++ src/EdpCommon/Util/String.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EdpCommon\Util;
+
+use COM;
+
+class String
+{
+    /**
+     * getRandomBytes
+     *
+     * returns X random raw binary bytes
+     *
+     * @param int $byteLength
+     * @return string
+     */
+    public static function getRandomBytes($byteLength)
+    {
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $data = openssl_random_pseudo_bytes($byteLength);
+        } elseif (is_readable('/dev/urandom')) {
+            $fp = fopen('/dev/urandom','rb');
+            if ($fp !== false) {
+                $data = fread($fp, $byteLength);
+                fclose($fp);
+            }
+        } elseif(function_exists('mcrypt_create_iv') && version_compare(PHP_VERSION, '5.3.0', '>=')) {
+            $data = mcrypt_create_iv($byteLength, MCRYPT_DEV_URANDOM);
+        } elseif (class_exists('COM')) {
+            try {
+                $capi = new COM('CAPICOM.Utilities.1');
+                $data = $capi->GetRandom($btyeLength,0);
+            } catch (\Exception $ex) {} // Fail silently
+        }
+        if(empty($data)) {
+            throw new \Exception(
+                'Unable to find a secure method for generating random bytes.'
+            );
+        }
+        return $data;
+    }
+}
