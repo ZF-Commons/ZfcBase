1c02c7446d12e7a2ffd08abbf901bc10b124aab4
Evan Coury <me@evancoury.com>
2012-01-08 12:44:29 -0700

    Complete refactoring for EdpCommon to ZfcBase renaming

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'ZfcBase\Module'                     => __DIR__ . '/Module.php',
-    'Zend\Db\Adapter\DiPdoMysql'           => __DIR__ . '/src/Zend/Db/Adapter/DiPdoMysql.php',
+    'Zend\Db\Adapter\DiPdoMysql'         => __DIR__ . '/src/Zend/Db/Adapter/DiPdoMysql.php',
     'ZfcBase\EventManager\EventProvider' => __DIR__ . '/src/ZfcBase/EventManager/EventProvider.php',
     'ZfcBase\Model\ModelAbstract'        => __DIR__ . '/src/ZfcBase/Model/ModelAbstract.php',
     'ZfcBase\Util\String'                => __DIR__ . '/src/ZfcBase/Util/String.php',
