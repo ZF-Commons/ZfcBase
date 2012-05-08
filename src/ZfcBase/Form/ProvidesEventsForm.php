653e3c674c350c9a9281fa41b619b22373328499
Evan Coury <me@evancoury.com>
2012-05-08 12:17:16 -0700

    Updated interface names for ZF2 master

--- src/ZfcBase/Form/ProvidesEventsForm.php
+++ src/ZfcBase/Form/ProvidesEventsForm.php
@@ -3,23 +3,23 @@
 namespace ZfcBase\Form;
 
 use Zend\Form\Form,
-    Zend\EventManager\EventCollection,
+    Zend\EventManager\EventManagerInterface,
     Zend\EventManager\EventManager;
 
 class ProvidesEventsForm extends Form
 {
     /**
-     * @var EventCollection
+     * @var EventManagerInterface
      */
     protected $events;
 
     /**
      * Set the event manager instance used by this context
      * 
-     * @param  EventCollection $events 
+     * @param  EventManagerInterface $events 
      * @return mixed
      */
-    public function setEventManager(EventCollection $events)
+    public function setEventManager(EventManagerInterface $events)
     {
         $this->events = $events;
         return $this;
@@ -30,11 +30,11 @@ class ProvidesEventsForm extends Form
      *
      * Lazy-loads an EventManager instance if none registered.
      * 
-     * @return EventCollection
+     * @return EventManagerInterface
      */
     public function events()
     {
-        if (!$this->events instanceof EventCollection) {
+        if (!$this->events instanceof EventManagerInterface) {
             $identifiers = array(__CLASS__, get_class($this));
             if (isset($this->eventIdentifier)) {
                 if ((is_string($this->eventIdentifier))
