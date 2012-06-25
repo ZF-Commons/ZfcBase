fbae141853d2bbdb54adede15a00a428add6d0a9
Evan Coury <me@evancoury.com>
2012-06-25 08:42:26 -0700

    Major cleanup
    - Removed a bunch of code that I'm unsure is needed
    - Fixed EventManagerAwareInterface

--- src/ZfcBase/Mapper/AbstractDbMapper.php
+++ src/ZfcBase/Mapper/AbstractDbMapper.php
@@ -2,19 +2,13 @@
 
 namespace ZfcBase\Mapper;
 
-use ReflectionProperty;
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\ResultSet\HydratingResultSet;
 use Zend\Db\Sql\Select;
-use Zend\Db\Sql\Sql;
-use ZfcBase\Mapper\MappingHydrator;
-use ZfcBase\Persistence\ObjectManagerInterface;
-use ZfcBase\Persistence\DefaultFinderInterface;
-use ZfcBase\Persistence\DefaultObjectManagerOptions;
+use Zend\Stdlib\Hydrator\HydratorInterface;
+use Zend\Stdlib\Hydrator\ClassMethods;
 
-abstract class AbstractDbMapper implements
-    DefaultFinderInterface,
-    ObjectManagerInterface
+abstract class AbstractDbMapper
 {
     /**
      * @var Adapter
@@ -22,7 +16,7 @@ abstract class AbstractDbMapper implements
     protected $dbAdapter;
 
     /**
-     * @var MappingHydrator
+     * @var HydratorInterface
      */
     protected $hydrator;
 
@@ -32,18 +26,14 @@ abstract class AbstractDbMapper implements
     protected $modelPrototype;
 
     /**
-     * @var DefaultObjectManagerOptions
+     * @var HydratingResultSet
      */
-    protected $options;
+    protected $resultSetPrototype;
 
     /**
-     * @return Select
+     * @var Select
      */
-    public function select()
-    {
-        $select = new Select();
-        return $select;
-    }
+    protected $selectPrototype;
 
     /**
      * @param Select $select
@@ -56,119 +46,10 @@ abstract class AbstractDbMapper implements
         $select->prepareStatement($adapter, $statement);
         $result = $statement->execute();
 
-        $resultSet = new \Zend\Db\ResultSet\HydratingResultSet();
-        $resultSet->setRowObjectHydrator($this->getHydrator());
-        $resultSet->setRowObjectPrototype($this->getModelPrototype());
-        $resultSet->setDataSource($result);
+        $resultSet = $this->getResultSet();
+        $resultSet->initialize($result);
 
         return $resultSet;
-    }
-
-    public function persist($object)
-    {
-        $className = $this->getOptions()->getClassName();
-        if (!$object instanceof $className) {
-            throw new Exception\InvalidArgumentException(
-                '$entity must be an instance of ' . $className
-            );
-        }
-        if ($this->getIdentifier($object)) {
-            $this->update($object);
-        } else {
-            $this->insert($object);
-        }
-    }
-
-    /**
-     * check if object has already an identifier
-     *
-     * @param object $object
-     * @return mixed
-     */
-    protected function getIdentifier($object)
-    {
-        $property = new ReflectionProperty(get_class($object), $this->getPrimaryKey());
-        $property->setAccessible(true);
-        return $property->getValue($object);
-    }
-
-    public function find($id)
-    {
-        $select = $this->select();
-        $select->from($this->getTableName());
-        $pkField = $this->getHydrator()->getFieldForProperty($this->getPrimaryKey());
-        $select->where(array($pkField => $id));
-
-        $results = $this->selectWith($select);
-        $row = null;
-        if ($results) {
-            $row = $results->current();
-        }
-        return $row;
-    }
-
-    /**
-     * @param object $object
-     * @return bool
-     */
-    public function remove($object)
-    {
-        $className = $this->getOptions()->getClassName();
-        if (!$object instanceof $className) {
-            throw new Exception\InvalidArgumentException(
-                '$entity must be an instance of ' . $className
-            );
-        }
-        if ($value = $this->getIdentifier($object)) {
-            $sql = new Sql($this->getDbAdapter(), $this->getTableName());
-            $delete = $sql->delete();
-            $pkField = $this->getHydrator()->getFieldForProperty($this->getPrimaryKey());
-            $delete->where(array($pkField => $value));
-            $statement = $sql->prepareStatementForSqlObject($delete);
-            $statement->execute();
-            return true;
-        }
-        return false;
-    }
-
-    protected function insert($entity)
-    {
-        $hydrator = $this->getHydrator();
-        $set = $hydrator->extract($entity);
-        $pkField = $this->getHydrator()->getFieldForProperty($this->getPrimaryKey());
-        unset($set[$pkField]);
-
-        $sql = new Sql($this->getDbAdapter(), $this->getTableName());
-        $insert = $sql->insert();
-        $insert->values($set);
-
-        $statement = $sql->prepareStatementForSqlObject($insert);
-        $result = $statement->execute();
-        $lastInsertValue = $this->getDbAdapter()->getDriver()->getConnection()->getLastGeneratedValue();
-
-        $property = new ReflectionProperty(get_class($entity), $this->getPrimaryKey());
-        $property->setAccessible(true);
-        $property->setValue($entity, $lastInsertValue);
-    }
-
-    protected function update($entity)
-    {
-        $pk = $this->getPrimaryKey();
-        $hydrator = $this->getHydrator();
-        $set = $hydrator->extract($entity);
-
-        $pkField = $this->getHydrator()->getFieldForProperty($pk);
-        $pkValue = $set[$pkField];
-        unset($set[$pkField]);
-
-        $sql = new Sql($this->getDbAdapter(), $this->getTableName());
-        $update = $sql->update();
-        $update->set($set);
-        $update->where(array($pkField => $pkValue));
-
-        $statement = $sql->prepareStatementForSqlObject($update);
-        $result = $statement->execute();
-        return $result->getAffectedRows();
     }
 
     /**
@@ -181,6 +62,16 @@ abstract class AbstractDbMapper implements
             $this->modelPrototype = new $className;
         }
         return $this->modelPrototype;
+    }
+
+    /**
+     * @param object $modelPrototype
+     * @return AbstractDbMapper
+     */
+    public function setModelPrototype($modelPrototype)
+    {
+        $this->modelPrototype = $modelPrototype;
+        return $this;
     }
 
     /**
@@ -202,111 +93,49 @@ abstract class AbstractDbMapper implements
     }
 
     /**
-     * @return MappingHydrator
+     * @return HydratorInterface
      */
     public function getHydrator()
     {
-        if (!$this->hydrator instanceof MappingHydrator) {
-            $map = $this->getOptions()->getMap();
-            $hydrator = new MappingHydrator($map);
-            $this->hydrator = $hydrator;
+        if (!$this->hydrator) {
+            $this->hydrator = new ClassMethods;
         }
         return $this->hydrator;
     }
 
     /**
-     * @return string
+     * @param HydratorInterface $hydrator
+     * @return AbstractDbMapper
      */
-    public function getPrimaryKey()
+    public function setHydrator(HydratorInterface $hydrator)
     {
-        return $this->getOptions()->getPrimaryKey();
+        $this->hydrator = $hydrator;
+        return $this;
     }
 
     /**
-     * @return string
+     * @return HydratingResultSet
      */
-    public function getTableName()
+    protected function getResultSet()
     {
-        return $this->getOptions()->getTableName();
-    }
-
-    /**
-     * Flush - not support by db mapper, does nothing
-     *
-     * @return void
-     */
-    public function flush($object = null, array $options = array())
-    {
-        // not supported in Db Mapper, do nothing
-    }
-
-    /**
-     * Finds all objects in the repository.
-     *
-     * @return mixed The objects.
-     */
-    public function findAll()
-    {
-        // TODO: Implement findAll() method.
-    }
-
-    /**
-     * Finds objects by a set of criteria.
-     *
-     * Optionally sorting and limiting details can be passed. An implementation may throw
-     * an UnexpectedValueException if certain values of the sorting or limiting details are
-     * not supported.
-     *
-     * @param array $criteria
-     * @param array|null $orderBy
-     * @param int|null $limit
-     * @param int|null $offset
-     * @return mixed The objects.
-     */
-    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-    {
-        // TODO: Implement findBy() method.
-    }
-
-    /**
-     * Finds a single object by a set of criteria.
-     *
-     * @param array $criteria
-     * @return object The object.
-     */
-    public function findOneBy(array $criteria)
-    {
-        $mappedCriteria = array();
-        foreach ($criteria as $property => $value) {
-            $field = $this->getHydrator()->getFieldForProperty($property);
-            $mappedCriteria[$field] = $value;
+        if (!$this->resultSetPrototype) {
+            $this->resultSetPrototype = new HydratingResultSet;
+            $this->resultSetPrototype->setHydrator($this->getHydrator());
+            $this->resultSetPrototype->setObjectPrototype($this->getModelPrototype());
         }
-        $select = $this->select();
-        $select->from($this->getTableName());
-        $select->where($mappedCriteria);
-        $results = $this->selectWith($select);
-        $row = null;
-        if ($results) {
-            $row = $results->current();
+        return clone $this->resultSetPrototype;
+    }
+
+    /**
+     * select
+     *
+     * @return Select
+     */
+    protected function select()
+    {
+        if (!$this->selectPrototype) {
+            $this->selectPrototype = new Select;
         }
-        return $row;
+        return clone $this->selectPrototype;
     }
-
-    /**
-     * @param DefaultObjectManagerOptions $options
-     */
-    public function setOptions(DefaultObjectManagerOptions $options)
-    {
-        $this->options = $options;
-    }
-
-    /**
-     * @return DefaultObjectManagerOptions
-     */
-    public function getOptions()
-    {
-        return $this->options;
-    }
-
-
 }
